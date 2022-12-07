2fa13410c50b17519f6f1eba3cf03d3686542830
patrcoff <90782615+patrcoff@users.noreply.github.com>
2022-12-07 17:59:16 +0000

    working proof of concept (no user config)
    submenu item passes menu obj not dict key to called function (test_func)
    changed test_func to reference dict[menu_object.text]
    (.text attribute of menu object is the important part here)

--- source/prototyping/app.py
+++ source/prototyping/app.py
@@ -1,12 +1,16 @@
 from tkinter import *
 from functools import partial
-#from task_queue import *
+from task_queue import *
 from pystray import Menu, MenuItem as item
 import pystray
 #import json
 #import re
 from PIL import Image, ImageTk
 
+commands = {'TASK1':['LINESMANYTOONE','STRINGTOLIST:,','REMOVEEMPTY','LISTTOSTRING: | '],
+'TASK2':['LINESMANYTOONE','STRINGTOLIST:,','REMOVEEMPTY','LISTTOSTRING: | '],
+'TASK3':['LINESMANYTOONE','STRINGTOLIST:,','REMOVEEMPTY','LISTTOSTRING: | '],
+'TASK4':['LINESMANYTOONE','STRINGTOLIST:,','REMOVEEMPTY','LISTTOSTRING: | ']}
 
 #-------------------------------------------------------------------------------
 #GUI SECTION
@@ -29,82 +33,29 @@ def show_window(icon, item):
     icon.stop()
     win.after(0,win.deiconify())
 
-def test_func(icon,item,x):#SO THIS WAS THE BLOODY PROBLEM, pystray item IS PASSING ITSELF AND ICON INTO THE FUNCTION, EVEN THOUGH IT'S IN A PARTIAL...?
-    print(x)
-
-lst = ['LINESMANYTOONE','STRINGTOLIST:,','REMOVEEMPTY','LISTTOSTRING: | ']
-
-# Hide the window and show on the system taskbar
-#func_lst = [item(x,partial(test_func,x) for x in lst]
-#menu2 = tuple(func_lst)
+def test_func(icon,item,queue):#SO THIS WAS THE BLOODY PROBLEM, pystray item IS PASSING ITSELF AND ICON INTO THE FUNCTION, EVEN THOUGH IT'S IN A PARTIAL...?
+    in_text = pyperclip.paste()
+    pyperclip.copy(run_queue(parser(commands[queue.text]),in_text))#BECAUSE THE SUBMENU ITEMS ARE PASSING THE MENU OBJECTS TO THIS FUNCTION, INSTEAD OF JUST THE STRING NAME OF THE KEY FROM THE DICT, WE NEED TO USE THE .text attr OF MENU OBJ
+    #print(type(queue),queue.text)
+    #print(commands[queue.text])
+lst = ['TASK1','TASK2','TASK3','TASK4']
 
 
-#THIS IS NOT YET WORKING AS INTENDED
 #I am trying to generate a dropdown menu dynamically which contains the name of the command list and when clicked calls the function which runs said command list.
-#currently trying to make a proof of concept outside of the task_queue (hence commented out import) and just get the menu items to print the sting from a list
-#but it is instead printing all the items in the list, 5 times over...
-#there's only 4 items in the list too so I really don't understand why, the menu dropdown labels are correctly showing only once
-#documentation seems to sparse for this specific intended use case, may require to use a different library
-#next thing I'm going to try and do is disregard the below and try and get a dict of key;value pairs, where value is a callable and just manually call these values by key reference to ensure I'm understanding the lambda part ok
 
-"""
-def generate_funcs_dict(lst):
-    dict = {}
-    for el in lst:
-        print(el)
-        new_lam = partial(test_func,el)
-        dict[el] = new_lam
-    return dict
-
-print(dict)
-
-functions = generate_funcs_dict(lst)
-
-
-tmp = functions['LINESMANYTOONE']
-input()
-tmp()
-
-print("------------------------------------\n\n\n")
-
-for el in functions:
-    functions[el]()
-print("------------------------------------\n\n\n")
-"""
-#lst_funcs = [partial(test_func,x) for x in lst]
-
-#print(lst_funcs)
-
-#for el in lst_funcs:
-#    el()
-#items = tuple(item(x,partial(test_func,x)) for x in lst)
-submenu = Menu(lambda: (item(x,partial(test_func,x)) for x in lst))
 def hide_window():
 
+    submenu = Menu(lambda: (item(x,partial(test_func,commands[x])) for x in commands))#I DON'T UNDERSTAND WHY BUT THIS IS NOT PASSING X (AS IN THE KEY FROM DICT COMMANDS) TO TEST_FUNC, BUT INSTEAD IS PASSING THE MENU OBJECT...
     win.withdraw()
     image=Image.open("favicon.ico")
     menu=(item('Quit', quit_window),
-        #item('Lines: Many to one', lines_many_to_one),#some inbuilt commands do not need chaining if they directly map to a single function (but should we allo this?)
-        item('Tasks:',submenu))#WE NEED TO MAKE A DROP DOWN LIST OF COMMANDS AVAILABLE IN THE available_commands DICT  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        #item('Lines: Many to one', lines_many_to_one),#some inbuilt commands do not need chaining if they directly map to a single function (but should we alloW this?)
+        item('Tasks:',submenu),
+        item('Quit!',quit_window))
     icon=pystray.Icon("name", image, "My System Tray Icon", menu)
     icon.run()
 
 
-#disregard below! (the below does work but now we got above to work)
-#I'm not really sure what got it working but I think it was setting submenu outside of the menu declaration so we passed a full (something) to the item entry for 'Tasks'
-#plus the addition of passing icon and item to the function I'm calling - probably something to do with decorators inside the menu class or some shit?
-
-"""
-#the below works! it seems pystray is using a slightly different object for systray menu items as passed to icon, and submenu items as called as Menu objects
-#I'm able to use a list here of the functions mapped to the command string as menu expects and iterable
-#in the above, when trying to create a sub menu, it doesn't work this way though, not expecting an iterable for a Menu object... perhaps if I make a list of menu objects?
-def hide_window():
-    win.withdraw()
-    image=Image.open("favicon.ico")
-    #WE NEED TO MAKE A DROP DOWN LIST OF COMMANDS AVAILABLE IN THE available_commands DICT  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    pystray.Icon("name", image, "My System Tray Icon", menu=[item(x,partial(test_func,x)) for x in lst]).run()
-    #icon.run()
-"""
 win.protocol('WM_DELETE_WINDOW', hide_window)
 
 win.mainloop()
