8b43da3eca718f1488232d7ccb1c086ddbb267b9
patrcoff <90782615+patrcoff@users.noreply.github.com>
2022-12-11 17:49:41 +0000

    removing unnecessary references to objects contained in self
    clearing a few more objects being passed directly to methods which are now contained within self after migration from flat -> functions -> object structure of App()

--- source/prototyping/__main__.py
+++ source/prototyping/__main__.py
@@ -18,6 +18,10 @@ class App():
         self.win.title("Clippy2000")
         # Set the size of the window
         self.win.geometry("700x350")
+        self.commands = {'TASK1':['LINESMANYTOONE','STRINGTOLIST:,','REMOVEEMPTY','LISTTOSTRING: | '],
+        'TASK2':['LINESMANYTOONE','STRINGTOLIST:,','REMOVEEMPTY','LISTTOSTRING: | '],
+        'TASK3':['LINESMANYTOONE','STRINGTOLIST:,','REMOVEEMPTY','LISTTOSTRING: | '],
+        'TASK4':['STRINGTOLIST:\n','REMOVEEMPTY','LISTTOTABLE','REVERSETABLE','GETCOLUMN:KB','LISTTOSTRING:\n']}
 
     def process_queue(self,queue_ref,icon,item):#SO THIS WAS THE BLOODY PROBLEM, pystray item IS PASSING ITSELF AND ICON INTO THE FUNCTION, EVEN THOUGH IT'S IN A PARTIAL...?
         in_text = pyperclip.paste()
@@ -33,7 +37,7 @@ class App():
         self.icon.stop()
         self.win.after(0,self.win.deiconify())
 
-    def hide_window(self,win,commands):
+    def hide_window(self,commands):
 
         submenu = Menu(lambda: (item(x,partial(self.process_queue,x)) for x in commands))#I DON'T UNDERSTAND WHY BUT THIS IS NOT PASSING X (AS IN THE KEY FROM DICT COMMANDS) TO process_queue, BUT INSTEAD IS PASSING THE MENU OBJECT...
         self.win.withdraw()
@@ -46,19 +50,17 @@ class App():
         self.icon.run()
 
     def get_commands(self):#THIS WILL BE USED TO GET FROM USER SETTINGS
-        commands = {'TASK1':['LINESMANYTOONE','STRINGTOLIST:,','REMOVEEMPTY','LISTTOSTRING: | '],
-        'TASK2':['LINESMANYTOONE','STRINGTOLIST:,','REMOVEEMPTY','LISTTOSTRING: | '],
-        'TASK3':['LINESMANYTOONE','STRINGTOLIST:,','REMOVEEMPTY','LISTTOSTRING: | '],
-        'TASK4':['STRINGTOLIST:\n','REMOVEEMPTY','LISTTOTABLE','REVERSETABLE','GETCOLUMN:KB','LISTTOSTRING:\n']}
-        if self.count == 3:
-            commands['Additional!'] = ['LINESMANYTOONE','STRINGTOLIST:,','REMOVEEMPTY','LISTTOSTRING: | ']
-        return commands
+        #commands = self.commands
+        if self.count == 2:
+            self.commands['Additional!'] = ['LINESMANYTOONE','STRINGTOLIST:,','REMOVEEMPTY','LISTTOSTRING: | ']
+        self.count += 1
+        return self.commands
 
     def run_app(self):
         commands = self.get_commands()
         #THE ABOVE IS TO BE REPLACED WITH A USER CONFIG LOADING/SAVING MECHANISM
 
-        self.win.protocol('WM_DELETE_WINDOW', partial(self.hide_window,self.win,commands))#when window is closed by user, the hide_window function runs (the systray function)
+        self.win.protocol('WM_DELETE_WINDOW', partial(self.hide_window,commands))#when window is closed by user, the hide_window function runs (the systray function)
 
         self.win.mainloop()
 
