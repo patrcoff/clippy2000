3c8abdf5b540af3ca0bc5897b7b277a9ac842f02
patrcoff <90782615+patrcoff@users.noreply.github.com>
2022-12-28 17:38:21 +0000

    black run

--- source/prototyping/app.py
+++ source/prototyping/app.py
@@ -1,19 +1,19 @@
-#from tkinter import *
+# from tkinter import *
 from functools import partial
 from task_queue import *
 import sys
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 
-#from pystray import Menu, MenuItem as item
-#import pystray
-#import json
-#import re
-#from PIL import Image, ImageTk
+# from pystray import Menu, MenuItem as item
+# import pystray
+# import json
+# import re
+# from PIL import Image, ImageTk
 
 
 class App(QMainWindow):
-    '''
+    """
     The main window class for the QT based GUI app of \'clipp2000\' - inherits from QMainWindow
 
     ...
@@ -39,12 +39,15 @@ class App(QMainWindow):
     Methods
     -------
     load_config()
-    '''
+    """
+
     def __init__(self):
         super(App, self).__init__()
-        #,config=Config(pathlib.Path(r'./'))
-        self.config = Config(pathlib.Path(r'./'))#will impliment different defaults for different OS's later
-        #for now is just the directory it runs from which should be source/prototyping for now
+        # ,config=Config(pathlib.Path(r'./'))
+        self.config = Config(
+            pathlib.Path(r"./")
+        )  # will impliment different defaults for different OS's later
+        # for now is just the directory it runs from which should be source/prototyping for now
         self.taskQueue = TaskQueue()
         self.initUI()
         self.debug = True
@@ -57,67 +60,80 @@ class App(QMainWindow):
 
     def save_config():
         pass
-    #load config
-    #save config (config will be handled in task_queue module and called here to keep sepatation between the text processing and config saving, and interface sides)
 
-    #def add_sys_menu(self,menu,tray):
+    # load config
+    # save config (config will be handled in task_queue module and called here to keep sepatation between the text processing and config saving, and interface sides)
 
-    def initUI(self):#we need to modify this to create our editor window
+    # def add_sys_menu(self,menu,tray):
+
+    def initUI(self):  # we need to modify this to create our editor window
 
         icon = QIcon("favicon.ico")
         self.tray = QSystemTrayIcon(self)
         self.tray.setIcon(icon)
         self.m = QMenu()
-        
-        self.first = QAction('edit menu')
+
+        self.first = QAction("edit menu")
         self.first.triggered.connect(self.onClick)
         self.m.addAction(self.first)
-        
-        self.second = QAction('refresh tray')
+
+        self.second = QAction("refresh tray")
         self.second.triggered.connect(self.refreshTrayMenu)
         self.m.addAction(self.second)
-        
 
         self.tray.setContextMenu(self.m)
-        
+
         self.tray.show()
         textEdit = QTextEdit()
         self.setCentralWidget(textEdit)
-        #-----------------------------------------------------------------------
+        # -----------------------------------------------------------------------
         p = QPushButton("Click Me", self)
         self.setCentralWidget(p)
-        p.clicked.connect(self.onClick)#PLACEHOLDER FOR EDITOR WINDOW
+        p.clicked.connect(self.onClick)  # PLACEHOLDER FOR EDITOR WINDOW
 
     def onClick(self):
-        print('YAY!')
-        #self.m.clear()
-        #use the above if we're building the full menu from scratch (we probably will, unless we can address them by index?)
-        #self.m.addAction('First')
-        self.m.addAction('Third')
-        #self.tray.setContextMenu(self.m)
-        #calling setContextMenu multiple times breaks the systray icon for some reason... just don't do it!
-    
-    def interim(self,var):#testing below functionality before creating intended function to call taskqueue
-        #print(var)
-        in_text = pyperclip.paste()
-        pyperclip.copy(self.taskQueue.run_queue(self.taskQueue.parser(self.config.task_queue[var]),in_text,debug=self.debug))
+        print("YAY!")
+        # self.m.clear()
+        # use the above if we're building the full menu from scratch (we probably will, unless we can address them by index?)
+        # self.m.addAction('First')
+        self.m.addAction("Third")
+        # self.tray.setContextMenu(self.m)
+        # calling setContextMenu multiple times breaks the systray icon for some reason... just don't do it!
 
+    def interim(
+        self, var
+    ):  # testing below functionality before creating intended function to call taskqueue
+        # print(var)
+        in_text = pyperclip.paste()
+        pyperclip.copy(
+            self.taskQueue.run_queue(
+                self.taskQueue.parser(self.config.task_queue[var]),
+                in_text,
+                debug=self.debug,
+            )
+        )
 
     def refreshTrayMenu(self):
-        
+
         self.m.clear()
         self.m.addAction(self.first)
 
-        #then read data structure object containing the list of command queues and their names
+        # then read data structure object containing the list of command queues and their names
         self.load_config()
 
-        self.objectList = []#have figured out why QAction objects needed to be saved to an attribute of the main object
-        #the addAction doesn't pass in a copy of an object, but a reference to an object, so the object (QAction) needs to 
-        #keep existing - therefore we cann add these objects to a list which is itself an attribute of self - then they are
-        #always available and don't get lost in the method scope
-        for taskQueue in self.config.task_queue:#this is only adding the last one, not using self.com doesn't work though
+        self.objectList = (
+            []
+        )  # have figured out why QAction objects needed to be saved to an attribute of the main object
+        # the addAction doesn't pass in a copy of an object, but a reference to an object, so the object (QAction) needs to
+        # keep existing - therefore we cann add these objects to a list which is itself an attribute of self - then they are
+        # always available and don't get lost in the method scope
+        for (
+            taskQueue
+        ) in (
+            self.config.task_queue
+        ):  # this is only adding the last one, not using self.com doesn't work though
             com = QAction(taskQueue)
-            com.triggered.connect(partial(self.interim,taskQueue))
+            com.triggered.connect(partial(self.interim, taskQueue))
             self.objectList.append(com)
         for i in range(len(self.objectList)):
             self.m.addAction(self.objectList[i])
@@ -126,9 +142,8 @@ class App(QMainWindow):
     def show_win(self):
         self.show()
 
-    #build systray outside of class - doesn't work in class for some reason...
-    #https://www.pythonguis.com/tutorials/pyqt6-system-tray-mac-menu-bar-applications/
-
+    # build systray outside of class - doesn't work in class for some reason...
+    # https://www.pythonguis.com/tutorials/pyqt6-system-tray-mac-menu-bar-applications/
 
 
 def main():
@@ -136,6 +151,7 @@ def main():
     app.setQuitOnLastWindowClosed(False)
     win = App()
     sys.exit(app.exec())
+
 
 """
 def main():
@@ -167,6 +183,6 @@ def main():
     app.exec()
 """
 
-if __name__ == '__main__':
-    #print('ismain')
+if __name__ == "__main__":
+    # print('ismain')
     main()
