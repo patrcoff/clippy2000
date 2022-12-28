6bba4312416e187158adb7036a6aeee7e4c44b23
patrcoff <90782615+patrcoff@users.noreply.github.com>
2022-12-28 18:06:18 +0000

    moved QT app to __main__.py

--- source/prototyping/__main__.py
+++ source/prototyping/__main__.py
@@ -1,126 +1,156 @@
-from tkinter import *
 from functools import partial
 from task_queue import *
-from pystray import Menu, MenuItem as item
-import pystray
+import sys
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
 
+# from pystray import Menu, MenuItem as item
+# import pystray
 # import json
 # import re
-from PIL import Image, ImageTk
-
-# this is branch GUI_QT - main will be replaced here once app.py is reqwritted in QT
-# this is left here for now for testing
-taskObj = TaskQueue()
+# from PIL import Image, ImageTk
 
 
-class App:
-    def __init__(self, debug=False):
-        self.debug = debug  # not currently passed to functions!
-        self.count = 0
-        self.win = Tk()
+class App(QMainWindow):
+    """
+    The main window class for the QT based GUI app of \'clipp2000\' - inherits from QMainWindow
 
-        self.win.title("Clippy2000")
-        # Set the size of the window
-        self.win.geometry("700x350")
-        self.commands = {
-            "TASK1": [
-                "LINESMANYTOONE",
-                "STRINGTOLIST:,",
-                "REMOVEEMPTY",
-                "LISTTOSTRING: | ",
-                "STRIPWHITESPACE",
-            ],
-            "CodefromRP": [
-                "STRINGTOLIST:\n",
-                "FOREACH",
-                "REMOVE:>>> ",
-                "REMOVE:... ",
-                "ENDFOR",
-                "LISTTOSTRING:\n",
-            ],  # this is for copying code from realpython where lines start with the repl >>> and ... symbols
-            "TASK3": [
-                "LINESMANYTOONE",
-                "STRINGTOLIST:,",
-                "REMOVEEMPTY",
-                "LISTTOSTRING: | ",
-            ],
-            "TASK4": [
-                "STRINGTOLIST:\n",
-                "REMOVEEMPTY",
-                "LISTTOTABLE",
-                "REVERSETABLE",
-                "GETCOLUMN:KB",
-                "LISTTOSTRING:\n",
-            ],
-        }
+    ...
+    Attributes
+    ----------
+    config : task_queue.Config
+        config object of task_queue module which contains saved task queues
+    taskQueue : task_queue.TaskQueue
+        TaskQueue object which holds the available tasks as well as methods to parse and run task queues
+    debug : bool
+        Used to determine whether user debug mode is used by TaskQueue object methods
+    tray : QSystemTrayIcon
+        System tray object from PyQt
+    first : QAction
+        The first entry in the system tray menu
+    second : QAction
+        The last entry in the system tray menu
+    m : QMenu
+        The menu object of the system tray icon
+    objectList : list
+        A dynamically populated list of QAction menu objects populated from user saved task queues in self.config
 
-    def process_queue(
-        self, queue_ref, icon, item
-    ):  # SO THIS WAS THE BLOODY PROBLEM, pystray item IS PASSING ITSELF AND ICON INTO THE FUNCTION, EVEN THOUGH IT'S IN A PARTIAL...?
+    Methods
+    -------
+    load_config()
+    """
+
+    def __init__(self):
+        super(App, self).__init__()
+        # ,config=Config(pathlib.Path(r'./'))
+        self.config = Config(
+            pathlib.Path(r"./")
+        )  # will impliment different defaults for different OS's later
+        # for now is just the directory it runs from which should be source/prototyping for now
+        self.taskQueue = TaskQueue()
+        self.initUI()
+        self.debug = True
+
+    def print_config(self):
+        print(self.config.task_queue)
+
+    def load_config(self):
+        self.config.load_task_queue()
+
+    def save_config():
+        pass
+
+    # load config
+    # save config (config will be handled in task_queue module and called here to keep sepatation between the text processing and config saving, and interface sides)
+
+    # def add_sys_menu(self,menu,tray):
+
+    def initUI(self):  # we need to modify this to create our editor window
+
+        icon = QIcon("favicon.ico")
+        self.tray = QSystemTrayIcon(self)
+        self.tray.setIcon(icon)
+        self.m = QMenu()
+
+        self.first = QAction("edit menu")
+        self.first.triggered.connect(self.onClick)
+        self.m.addAction(self.first)
+
+        self.second = QAction("refresh tray")
+        self.second.triggered.connect(self.refreshTrayMenu)
+        self.m.addAction(self.second)
+
+        self.tray.setContextMenu(self.m)
+
+        self.tray.show()
+        textEdit = QTextEdit()
+        self.setCentralWidget(textEdit)
+        # -----------------------------------------------------------------------
+        p = QPushButton("Click Me", self)
+        self.setCentralWidget(p)
+        p.clicked.connect(self.onClick)  # PLACEHOLDER FOR EDITOR WINDOW
+
+    def onClick(self):
+        print("YAY!")
+        # self.m.clear()
+        # use the above if we're building the full menu from scratch (we probably will, unless we can address them by index?)
+        # self.m.addAction('First')
+        self.m.addAction("Third")
+        # self.tray.setContextMenu(self.m)
+        # calling setContextMenu multiple times breaks the systray icon for some reason... just don't do it!
+
+    def interim(
+        self, var
+    ):  # testing below functionality before creating intended function to call taskqueue
+        # print(var)
         in_text = pyperclip.paste()
-        # commands = self.get_commands()
         pyperclip.copy(
-            taskObj.run_queue(
-                taskObj.parser(self.commands[queue_ref]), in_text, debug=self.debug
+            self.taskQueue.run_queue(
+                self.taskQueue.parser(self.config.task_queue[var]),
+                in_text,
+                debug=self.debug,
             )
         )
 
-    def quit_window(self, icon, item):
-        self.icon.stop()
-        self.win.destroy()
+    def refreshTrayMenu(self):
 
-    def show_window(self, icon, item):
-        self.icon.stop()
-        self.win.after(0, self.win.deiconify())
+        self.m.clear()
+        self.m.addAction(self.first)
 
-    def hide_window(self):
-        print("Running hide_window")
-        self.get_commands()  # refresh the commands lists
-        submenu = Menu(
-            lambda: (item(x, partial(self.process_queue, x)) for x in self.commands)
-        )
-        self.win.withdraw()
-        image = Image.open("favicon.ico")
-        menu = (
-            item("Editor", self.show_window),
-            # item('Lines: Many to one', lines_many_to_one),#some inbuilt commands do not need chaining if they directly map to a single function (but should we alloW this?)
-            item("Tasks:", submenu),
-            item("Quit!", self.quit_window),
-        )
-        self.icon = pystray.Icon("name", image, "My System Tray Icon", menu)
-        self.icon.run()
+        # then read data structure object containing the list of command queues and their names
+        self.load_config()
 
-    def get_commands(self):  # THIS WILL BE USED TO GET FROM USER SETTINGS FROM FILE
-        # commands = self.commands
+        self.objectList = (
+            []
+        )  # have figured out why QAction objects needed to be saved to an attribute of the main object
+        # the addAction doesn't pass in a copy of an object, but a reference to an object, so the object (QAction) needs to
+        # keep existing - therefore we cann add these objects to a list which is itself an attribute of self - then they are
+        # always available and don't get lost in the method scope
+        for (
+            taskQueue
+        ) in (
+            self.config.task_queue
+        ):  # this is only adding the last one, not using self.com doesn't work though
+            com = QAction(taskQueue)
+            com.triggered.connect(partial(self.interim, taskQueue))
+            self.objectList.append(com)
+        for i in range(len(self.objectList)):
+            self.m.addAction(self.objectList[i])
+        self.m.addAction(self.second)
 
-        # FOR NOW WE'RE JUST CHECKING IF IT WILL WORK TO EDIT THE SUBMENUS AFTER CLOSNING THE EDITOR EACH TIME
-        if self.count == 2:
-            print("We've clicking this twice!")
-            self.commands["Additional!"] = [
-                "LINESMANYTOONE",
-                "STRINGTOLIST:,",
-                "REMOVEEMPTY",
-                "LISTTOSTRING: | ",
-            ]
-        self.count += 1
-        # return self.commands
+    def show_win(self):
+        self.show()
 
-    def run_app(self):
-        # commands = self.get_commands()
-        # THE ABOVE IS TO BE REPLACED WITH A USER CONFIG LOADING/SAVING MECHANISM
-
-        self.win.protocol(
-            "WM_DELETE_WINDOW", partial(self.hide_window)
-        )  # when window is closed by user, the hide_window function runs (the systray function)
-
-        self.win.mainloop()
+    # build systray outside of class - doesn't work in class for some reason...
+    # https://www.pythonguis.com/tutorials/pyqt6-system-tray-mac-menu-bar-applications/
 
 
 def main():
-    app = App(debug=True)
-    app.run_app()
-
+    app = QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(False)
+    win = App()
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
-
+    # print('ismain')
     main()
